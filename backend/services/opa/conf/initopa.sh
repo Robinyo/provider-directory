423b7a12f922dd5e0ce7648033c603124468cab8
Rob Ferguson <hey@rob-ferguson.me>
2025-07-13 08:58:46 +1000

    Updated the Organization policy

--- backend/services/opa/conf/initopa.sh
+++ backend/services/opa/conf/initopa.sh
@@ -19,13 +19,34 @@ curl $CURL_OPTS -X PUT "${OPA_SERVICE_PROTOCOL}://${OPA_SERVICE_HOST}:${OPA_SERV
   --header 'Content-Type: text/plain' \
   --data-binary 'package organization.read
 
-import rego.v1
-
 methods := "GET HEAD"
+path := "/fhir/Organization"
+scope := "system/Organization.read"
 
 default allow := false
 
-allow if contains(methods, input.request.method)'
+allow if {
+  is_method
+	is_request_path
+	is_scope
+	is_client
+}
+
+is_method if contains(methods, input.request.method)
+is_request_path if input.request.path == path
+is_scope if contains(token.scope, scope)
+
+is_client if token.azp == "oauth2-proxy"
+
+bearer_token := t if {
+	v := input.request.headers.authorization
+	startswith(v, "Bearer ")
+	t := substring(v, count("Bearer "), -1)
+}
+
+token := payload if {
+	[_, payload, _] := io.jwt.decode(bearer_token)
+}'
 
 # organization.write
 
