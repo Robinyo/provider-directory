82d23d4669c3dd3d77c2038b1c9404703f83064c
Rob Ferguson <hey@rob-ferguson.me>
2025-07-15 07:52:36 +1000

    Updated the Quick Start Guide

--- backend/services/opa/conf/initopa.sh
+++ backend/services/opa/conf/initopa.sh
@@ -8,10 +8,6 @@ export OPA_SERVICE_HOST=${OPA_SERVICE_HOST:-opa}
 export OPA_SERVICE_ADMIN_PORT=${OPA_SERVICE_ADMIN_PORT:-8181}
 # export OPA_SERVICE_ADMIN_PORT=${OPA_SERVICE_ADMIN_PORT:-8282}
 
-# CURL_OPTS=" --silent --show-error --fail"
-# CURL_OPTS=" --silent --show-error --fail --insecure"
-# CURL_OPTS=" --location --request --silent --show-error --fail"
-
 # Policies
 
 # organization.read
